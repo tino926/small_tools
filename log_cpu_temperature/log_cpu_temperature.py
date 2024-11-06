be6421bf585af5ff25546aec5fb8a1e8c69c2028
tino <tino926@users.noreply.github.com>
2024-11-06 20:39:39 +0800

    add the actual command

--- log_cpu_temperature/log_cpu_temperature.py
+++ log_cpu_temperature/log_cpu_temperature.py
@@ -5,7 +5,7 @@ while True:
     # The command to get CPU temperature may vary depending on your OS
     # For example, on Linux, you can use 'sensors' command and parse the output
     # Here, I'm using a placeholder command, replace it with the actual command for your system
-    temp = os.popen('your_command_to_get_cpu_temp').read()
+    temp = os.popen('sensors | grep "Core 0" | awk \'{print $3}\'').read()
 
     with open('cpu_temp.txt', 'a') as f:
         f.write(temp + '\n')
